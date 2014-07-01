a5c7e6feb3dcb571ec2f9549a3920bc1c31d83b1
Tim Erwin <tim.erwin@gmail.com>
2014-07-01 02:59:07 -0400

    ...

--- app.js
+++ app.js
@@ -13,12 +13,71 @@ fs.readFile(filePath, {
   }
 });
 
-function player(name, hand) {
+var lookup = {
+  values: {
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+    "T": 10,
+    "J": 11,
+    "Q": 12,
+    "K": 13,
+    "A": 14
+  },
+  suits: ["D", "H", "S", "C"]
+};
+
+function Hand(hand) {
+  var self = this;
+
+  //hand = "5D 4D 6D 7D 8D";
+
+  self.hand = hand;
+  self.cards = hand.split(" ");
+
+  self.hasValues = function(combo) {
+
+  };
+
+  self.getValues = function() {
+    var c = self.cards;
+
+    return c[0][0] + c[1][0] + c[2][0] + c[3][0] + c[4][0];
+  };
+
+  self.getSuits = function() {
+    var c = self.cards;
+
+    return c[0][1] + c[1][1] + c[2][1] + c[3][1] + c[4][1];
+  };
+
+  self.isFlush = function() {
+    var suits = lookup.suits;
+
+    for (var i = 0; i < suits.length; i++) {
+      var s = suits[i];
+      
+      var match = self.getSuits() === Array(6).join(s);
+
+      if (match) return true;
+    }
+
+    return false;
+  };
+
+  return self;
+}
+
+function Player(name, hand) {
   return {
     name: name,
-    hand: hand,
-    cards: hand.split(" ")
-  }
+    hand: new Hand(hand)
+  };
 }
 
 var games = [];
@@ -29,18 +88,18 @@ function play(data) {
   for (var i = 0; i < deals.length; i++) {
     var deal = deals[i];
 
-    if (deal.length == 0)
+    if (deal.length === 0)
       continue;
 
-    var player1 = new player("player1", deal.substr(0, 14));
-    var player2 = new player("player2", deal.substr(15, 14));
+    var player1 = new Player("player1", deal.substr(0, 14));
+    var player2 = new Player("player2", deal.substr(15, 14));
 
-    var result = decide(player1,player2);
+    var result = decide(player1, player2);
 
     games.push(result);
   }
 }
 
-function decide(p1,p2){
-  console.log(p1.cards + "" + " : " + p2.cards+"");
+function decide(p1, p2) {
+  console.log(p1.hand.getSuits() + " : " + p2.hand.getValues());
 }
